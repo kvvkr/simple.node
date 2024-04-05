205b9fc7c2e6944fa407dd2997cbfe1c74e0371a
kvvkr <162404275+kvvkr@users.noreply.github.com>
2024-04-05 12:00:55 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,8 +3,7 @@ sudo su - << EOF
 sudo apt update -y
 # Install Git
 sudo apt install git -y
- git clone https://github.com/vickydevo/simple-node.git
- cd simple-node
+https://github.com/kvvkr/simple.node.git cd simple-node
 sudo apt install npm -y
 npm install -g pm2   
 npm install
